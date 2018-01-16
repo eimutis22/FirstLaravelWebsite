d77a1641872a1dbadbe8091d03bc9f7785eda968
s00161472 <s00161472@mail.itsligo.ie>
2018-01-16 14:44:10 +0000

    Put error check in Include

---
+++ resources/views/inc/messages.blade.php
@@ -0,0 +1,9 @@
+@section('messages')
+
+@if(count($errors) > 0)
+    @foreach($errors->all() as $error)
+        <div class="alert alert-danger">
+            {{$error}}
+        </div>
+    @endforeach
+@endif
