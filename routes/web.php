55899ce6f1dec12dffce940a5853ee518e47fba3
s00161472 <s00161472@mail.itsligo.ie>
2018-01-16 15:47:55 +0000

    Tidy Routes

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::get('/', 'PagesController@getHome');
 
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('/about', 'PagesController@getAbout');
 
-Route::get('/contact', function () {
-    return view('contact');
-});
+Route::get('/contact', 'PagesController@getContact');
 
 Route::get('/messages', 'MessagesController@getMessages');
 
