0e5b9acc838b42a456b9a702d99b911fd9fe73f1
s00161472 <s00161472@mail.itsligo.ie>
2018-01-16 16:16:23 +0000

    Nav active

--- resources/views/inc/navbar.blade.php
+++ resources/views/inc/navbar.blade.php
@@ -8,14 +8,17 @@
 
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
+      <li class="nav-item {{Request::is('/') ? 'active' : ''}}">
         <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{Request::is('about') ? 'active' : ''}}">
         <a class="nav-link" href="/about">About</a>
       </li>
-      <li class="nav-item">
+      <li class="nav-item {{Request::is('contact') ? 'active' : ''}}">
         <a class="nav-link" href="/contact">Contact</a>
+      </li>
+      <li class="nav-item {{Request::is('messages') ? 'active' : ''}}">
+        <a class="nav-link" href="/messages">Messages</a>
       </li>
     </ul>
     <form class="form-inline my-2 my-lg-0">
