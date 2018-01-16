a6ce4fef5c578f1c2b951f073c59b80e0278a7b5
s00161472 <s00161472@mail.itsligo.ie>
2018-01-16 14:21:39 +0000

    Contact Form - Laravel Collective

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -2,5 +2,5 @@
 @section('content')
 
 <h1>About</h1>
-
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur odit accusamus quod at natus, ratione iure eum recusandae earum doloribus reiciendis optio officia maiores, voluptate enim, quisquam suscipit eius magni.</p>
 @endsection
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,7 +2,7 @@
 @section('content')
 
 <h1>Home</h1>
-
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Error culpa impedit, dolorum quaerat natus explicabo cupiditate neque? Temporibus assumenda, similique aperiam, delectus minima iure nulla tempora possimus saepe corrupti quam.</p>
 @endsection
 
 @section('sidebar')
